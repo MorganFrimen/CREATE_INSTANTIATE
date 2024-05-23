d75b9514947d1dacf2bc5b3d0278399de3d94647
mr.morganfrimen@yandex.ru <Sergey>
2024-05-23 09:39:15 +0300

    plus

--- Source.cpp
+++ Source.cpp
@@ -8,11 +8,12 @@ private:
 
 public:
 	explicit Entity(const std::string& name)
-		: m_Name(name), m_Age(-1) {}
+		: m_Name(name), m_Age(0) {}
 	explicit Entity(int age)
 		: m_Name("Uuknow"), m_Age(age) {}
 
 	const std::string& GetName()const { return m_Name; }
+	int GetAge() { return m_Age; }
 };
 
 void PrintEntity(const Entity& entity)
@@ -22,8 +23,12 @@ void PrintEntity(const Entity& entity)
 
 int main()
 {
-	Entity a { "Hello" };
-	Entity b { 22 };
+	Entity* a = new Entity{ "Max Pain" };
+	Entity* b = new Entity{ 22 };
+	std::cout << "Name: " << a->GetName() << "\nAge: " << b->GetAge() << std::endl;
+
+	delete a;
+	delete b;
 
 	/*
 	int a{ 2 };
